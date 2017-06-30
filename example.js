c3497a492c6eb1b4742b566c488b880a24cea234
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-29 21:00:21 -0400

    run update and lint-deps

--- example.js
+++ example.js
@@ -1,30 +1,20 @@
 'use strict';
 
 var url = require('url');
-var extend = require('extend-shallow');
 var parse = require('./');
 
-var input = '- @doowb\n- @jonschlinkert';
+var input = 'Foo @doowb bar @jonschlinkert;';
 
 console.log('=== LIST OF MENTIONS ===');
 console.log(parse(input));
 console.log();
 
-console.log('=== LIST OF MENTIONS (stringify = false) ===');
-console.log(parse(input, {stringify: false}));
-console.log();
-
-console.log('=== REPLACE WITH CUSTOM FUNCTION ===');
+console.log('=== CUSTOM TRANSFORM FUNCTION ===');
 console.log(parse(input, replace('https://github.com')));
 console.log();
 
 function replace(prefix) {
-  return function(name) {
-    return `[@${name}](${join(prefix, name)})`;
+  return function(tok) {
+    return `[@${tok.name}](${url.resolve(prefix, tok.name)})`;
   };
-}
-
-function join(prefix, name) {
-  var obj = extend({}, url.parse(prefix), {pathname: name});
-  return url.format(obj);
 }
